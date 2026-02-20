b561e686679f24fd726a82cc4dab691a1ce0d4d3
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2026-02-20 13:09:25 +0530

    footer_links_made_active

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -8,12 +8,13 @@ import {CircleDot} from "lucide-react";
 import Link from "next/link";
 import {Tag} from "./clean";
 import {VscGithubAlt} from "react-icons/vsc";
+import { redirect } from "next/navigation";
 
 export default function Footer() {
   return (
     <footer className="my-10 mx-2 md:mx-0 flex flex-col items-center gap-6">
       <div className="flex items-center gap-2 md:gap-4">
-        <div className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
+        <Link href="mailto:mrityunjaytiwari1873@gmail.com" target="_blank" className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
           <Tooltip>
             <TooltipTrigger asChild>
               <MdMail className="text-white md:size-3 size-2" />
@@ -22,8 +23,8 @@ export default function Footer() {
               <p>Email</p>
             </TooltipContent>
           </Tooltip>
-        </div>
-        <div className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
+        </Link>
+        <Link href="https://www.linkedin.com/in/mrityunjay-tiwari-a81275190/" target="_blank" className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
           <Tooltip>
             <TooltipTrigger asChild>
               <FaLinkedinIn className="text-white md:size-3 size-2" />
@@ -32,8 +33,8 @@ export default function Footer() {
               <p>LinkedIn</p>
             </TooltipContent>
           </Tooltip>
-        </div>
-        <div className="md:px-2 ml-1 md:ml-2 hover:cursor-pointer border rounded-md border-neutral-100 dark:border-neutral-900 md:rounded-none px-1 md:border-r-0 md:border-l-0 md:border-t-0  md:border-b-2 py-1 md:border-transparent hover:border-b-2 hover:border-orange-200 hover:transition-all hover:duration-200">
+        </Link>
+        <Link href="https://twitter.com/mrityunjay_18" target="_blank" className="md:px-2 ml-1 md:ml-2 hover:cursor-pointer border rounded-md border-neutral-100 dark:border-neutral-900 md:rounded-none px-1 md:border-r-0 md:border-l-0 md:border-t-0  md:border-b-2 py-1 md:border-transparent hover:border-b-2 hover:border-orange-200 hover:transition-all hover:duration-200">
           <Tooltip>
             <TooltipTrigger asChild>
               <div className="flex items-center gap-0.5 md:gap-1  ">
@@ -45,8 +46,8 @@ export default function Footer() {
               <p>Twitter</p>
             </TooltipContent>
           </Tooltip>
-        </div>
-        <div className="md:px-2 mr-1 md:mr-2 hover:cursor-pointer border rounded-md border-neutral-100 dark:border-neutral-900 md:rounded-none px-1 md:border-r-0 md:border-l-0 md:border-t-0  md:border-b-2 py-1 md:border-transparent hover:border-b-2 hover:border-orange-200 hover:transition-all hover:duration-200">
+        </Link>
+        <Link href="https://github.com/mrityunjay-tiwari" target="_blank" className="md:px-2 mr-1 md:mr-2 hover:cursor-pointer border rounded-md border-neutral-100 dark:border-neutral-900 md:rounded-none px-1 md:border-r-0 md:border-l-0 md:border-t-0  md:border-b-2 py-1 md:border-transparent hover:border-b-2 hover:border-orange-200 hover:transition-all hover:duration-200">
           <Tooltip>
             <TooltipTrigger asChild>
               <div className="flex items-center gap-0.5 md:gap-1  ">
@@ -58,8 +59,8 @@ export default function Footer() {
               <p>Github</p>
             </TooltipContent>
           </Tooltip>
-        </div>
-        <div className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
+        </Link>
+        <Link href="https://medium.com/@mrityunjaytiwari1873" target="_blank" className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
           <Tooltip>
             <TooltipTrigger asChild>
               <FaMedium className="text-white md:size-3 size-2" />
@@ -68,8 +69,8 @@ export default function Footer() {
               <p>Medium</p>
             </TooltipContent>
           </Tooltip>
-        </div>
-        <div className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
+        </Link>
+        <Link href="https://peerlist.io/mrityunjay18" target="_blank" className="bg-neutral-300 dark:bg-neutral-700 hover:bg-gray-700 rounded-full p-1 hover:cursor-pointer hover:transition-all hover:duration-300">
           <Tooltip>
             <TooltipTrigger asChild>
               <SiPeerlist className="text-white md:size-3 size-2" />
@@ -78,7 +79,7 @@ export default function Footer() {
               <p>Peerlist</p>
             </TooltipContent>
           </Tooltip>
-        </div>
+        </Link>
       </div>
       <Link href="/contact">
         <div className="flex gap-1 items-center text-neutral-400 border-b-2 border-orange-100">
