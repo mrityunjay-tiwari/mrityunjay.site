cd4a3f1dd95d2b5cedd044525bcd801067fe4dd9
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2026-02-20 10:57:00 +0530

    accelerate_removed

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from '@/lib/generated/prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate' // you need not use @prisma/extension-accelerate if using neonDB
-
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
